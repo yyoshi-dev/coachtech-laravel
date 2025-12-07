c7720a1ed80b00d1308795171e4c2b9c9ce7c200
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2025-12-07 20:15:29 +0900

    ブラウザに画面を表示: View編

--- first-laravel/src/app/Http/Controllers/TestController.php
+++ first-laravel/src/app/Http/Controllers/TestController.php
@@ -6,14 +6,24 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
+    // 1-7, 1-8 ブラウザに画面を表示しよう:controller編及びroute編
     // public function index($text)
     // public function index($text = "デフォルト")
-    public function index(Request $request)
+    // public function index(Request $request)
+    // {
+    //     $item = [
+    //         'content' => 'パラメータを渡す',
+    //         // 'param' => $text
+    //         'param' => $request->text
+    //     ];
+    //     return view('index', $item);
+    // }
+
+    // 1-9 ブラウザに画面を表示しよう: view編
+    public function index()
     {
         $item = [
-            'content' => 'パラメータを渡す',
-            // 'param' => $text
-            'param' => $request->text
+            'content' => '本文'
         ];
         return view('index', $item);
     }
--- first-laravel/src/routes/web.php
+++ first-laravel/src/routes/web.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
-use App\Http\Controllers\HelloController;
+// use App\Http\Controllers\HelloController;
 
+// 1-7, 1-8 ブラウザに画面を表示しよう:controller編及びroute編
 // Route::get('/test', [TestController::class, 'index']);
 // Route::get('/test/{text}', [TestController::class, 'index']);
 // Route::get('/test/{text?}', [TestController::class, 'index']);
-Route::get('/test', [TestController::class, 'index']);
-Route::get('/hello', [HelloController::class, 'index']);
+// Route::get('/test', [TestController::class, 'index']);
+// Route::get('/hello', [HelloController::class, 'index']);
 
 // 問1
 // Route::get('/test/{room}/{id}', function ($room, $id) {
@@ -19,3 +20,6 @@ Route::get('/hello', [HelloController::class, 'index']);
 // Route::get('/test/{greeting?}', function ($greeting = 'Goodmorning') {
 //     return $greeting . '=おはようございます';
 // });
+
+// 1-9 ブラウザに画面を表示しよう: view編
+Route::get('/', [TestController::class, 'index']);
