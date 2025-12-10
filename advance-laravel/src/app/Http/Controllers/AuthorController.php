2550f89e3a5512ccec8558902801bf5f5d9640c2
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2025-12-11 00:38:00 +0900

    取得したデータ一覧を表示する画面を作成

---
+++ advance-laravel/src/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    public function index()
+    {
+        $authors = Author::all();
+        return view('index', ['authors' => $authors]);
+    }
+}
---
+++ advance-laravel/src/resources/views/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.default')
+<style>
+    th {
+        background-color: #289ADC;
+        color: white;
+        padding: 5px 40px;
+    }
+
+    tr:nth-child(odd) td {
+        background-color: #FFFFFF;
+    }
+
+    td {
+        padding: 25px 40px;
+        background-color: #EEEEEE;
+        text-align: center;
+    }
+</style>
+
+@section('title', 'index.blade.php')
+
+@section('content')
+<table>
+    <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>age</th>
+        <th>nationality</th>
+    </tr>
+    @foreach ($authors as $author)
+    <tr>
+        <td>{{$author->id}}</td>
+        <td>{{$author->name}}</td>
+        <td>{{$author->age}}</td>
+        <td>{{$author->nationality}}</td>
+    </tr>
+    @endforeach
+</table>
+@endsection
