09f8e9b867de7d10c2a7b10670f55f8afeabd75e
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2025-12-11 23:33:08 +0900

    データ更新機能を実装

--- advance-laravel/src/app/Http/Controllers/AuthorController.php
+++ advance-laravel/src/app/Http/Controllers/AuthorController.php
@@ -7,23 +7,40 @@ use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
+    // 取得データ一覧ページの表示
+    public function index()
+    {
+        $authors = Author::all();
+        return view('index', ['authors' => $authors]);
+    }
+
     // データ追加用ページの表示
     public function add()
     {
         return view('add');
     }
 
-    // リクエストからのデータ取得
+    // 追加機能
     public function create(Request $request)
     {
         $form = $request->all();
         Author::create($form);
         return redirect('/');
     }
-    // 取得データ一覧の表示
-    public function index()
+
+    // データ編集ページの表示
+    public function edit(Request $request)
     {
-        $authors = Author::all();
-        return view('index', ['authors' => $authors]);
+        $author = Author::find($request->id);
+        return view('edit', ['form' => $author]);
+    }
+
+    // 更新機能
+    public function update(Request $request)
+    {
+        $form = $request->all();
+        unset($form['_token']);
+        Author::find($request->id)->update($form);
+        return redirect(('/'));
     }
 }
