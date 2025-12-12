7a04dffd9de44e2cdbce5f2c535d5a2db47475c4
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2025-12-12 21:35:53 +0900

    belongs To結合

--- advance-laravel/src/app/Models/Book.php
+++ advance-laravel/src/app/Models/Book.php
@@ -14,6 +14,10 @@ class Book extends Model
 
     public function getTitle()
     {
-        return 'ID' . $this->id . ':' . $this->title;
+        return 'ID' . $this->id . ':' . $this->title . ' 著者:' . optional($this->author)->name;
+    }
+    public function author()
+    {
+        return $this->belongsTo('App\Models\Author');
     }
 }
