97ef6ffb313d00177a9053533eafb63d38648541
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2025-12-13 17:40:40 +0900

    N+1問題の解消

--- advance-laravel/src/app/Http/Controllers/BookController.php
+++ advance-laravel/src/app/Http/Controllers/BookController.php
@@ -9,7 +9,8 @@ class BookController extends Controller
 {
     public function index()
     {
-        $items = Book::all();
+        // $items = Book::all();
+        $items = Book::with('author')->get();
         return view('book.index', ['items' => $items]);
     }
 
