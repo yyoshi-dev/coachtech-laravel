475b7dc8e04df56f09611f183d21de34b1dfcaf9
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2025-12-16 21:34:17 +0900

    ビューファイルの作成

---
+++ third-laravel/src/resources/views/auth/login.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{asset('css/login.css')}}">
+@endsection
+
+@section('content')
+<div class="login-form__content">
+    <div class="login-form__heading">
+        <h2>ログイン</h2>
+    </div>
+    <form action="" class="form">
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">メールアドレス</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="email" name="email" value="{{old('email')}}">
+                </div>
+                <div class="form__error">
+                    @error('email')
+                    {{$message}}
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">パスワード</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="password" , name="password">
+                </div>
+                <div class="form__error">
+                    @error('password')
+                    {{$message}}
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">ログイン</button>
+        </div>
+    </form>
+    <div class="register__link">
+        <a href="/register" class="register__button-submit">会員登録の方はこちら</a>
+    </div>
+</div>
+@endsection
