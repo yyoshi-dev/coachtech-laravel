954d6fb527535310c821cd778dbf1dfb30aba7dc
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2025-12-20 00:17:40 +0900

    各データ挿入方法の確認

---
+++ advance-laravel/src/database/migrations/2025_12_19_232657_create_pens_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pens', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->string('name');
+            $table->integer('price');
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pens');
+    }
+};
--- advance-laravel/src/routes/web.php
+++ advance-laravel/src/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\PenController;
 use App\Models\Person;
 use App\Models\Product;
 
@@ -63,3 +64,8 @@ Route::get('uuid', function () {
         echo $product . '<br>';
     }
 });
+
+// Pen関連
+Route::get('fill', [PenController::class, 'fillPen']);
+Route::get('create', [PenController::class, 'createPen']);
+Route::get('insert', [PenController::class, 'insertPen']);
