0d666fab6bab3758d35f633cc8298962b8fec352
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2025-12-22 22:48:46 +0900

    バリデーションの追加

--- contact-form/src/resources/views/index.blade.php
+++ contact-form/src/resources/views/index.blade.php
@@ -18,10 +18,13 @@
             </div>
             <div class="form__group-content">
                 <div class="form__input--text">
-                    <input type="text" name="name" placeholder="テスト太郎">
+                    <input type="text" name="name" placeholder="テスト太郎" value="{{old('name')}}">
                 </div>
                 <div class="form__error">
                     <!-- バリデーション機能実装後に記述 -->
+                    @error('name')
+                    {{$message}}
+                    @enderror
                 </div>
             </div>
         </div>
@@ -32,10 +35,13 @@
             </div>
             <div class="form__group-content">
                 <div class="form__input--text">
-                    <input type="email" name="email" placeholder="test@example.com">
+                    <input type="email" name="email" placeholder="test@example.com" value="{{old('email')}}">
                 </div>
                 <div class="form__error">
                     <!-- バリデーション機能実装後に記述 -->
+                    @error('email')
+                    {{$message}}
+                    @enderror
                 </div>
             </div>
         </div>
@@ -46,10 +52,13 @@
             </div>
             <div class="form__group-content">
                 <div class="form__input--text">
-                    <input type="tel" name="tel" placeholder="09012345678">
+                    <input type="tel" name="tel" placeholder="09012345678" value="{{old('tel')}}">
                 </div>
                 <div class="form__error">
                     <!-- バリデーション機能実装後に記述 -->
+                    @error('tel')
+                    {{$message}}
+                    @enderror
                 </div>
             </div>
         </div>
