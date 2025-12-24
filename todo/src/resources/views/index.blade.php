07534a091d29b99e263d2d521bf428a732a205d9
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2025-12-25 00:47:10 +0900

    追加機能を実装

--- todo/src/resources/views/index.blade.php
+++ todo/src/resources/views/index.blade.php
@@ -18,10 +18,11 @@
     <div class="todo__list">
         <h2 class="todo__list-title">Todo</h2>
         <ul class="todo__items">
+            @foreach ($todos as $todo)
             <li class="todo__item">
                 <form action="/todos/update" method="post" class="todo__update-form">
                     @csrf
-                    <input type="text" name="content" value="test" class="todo__item-input">
+                    <input type="text" name="content" value={{$todo['content']}} class="todo__item-input">
                     <button type="submit" class="todo__item-update">更新</button>
                 </form>
                 <form action="/todos/delete" method="post" class="todo__delete-form">
@@ -29,17 +30,7 @@
                     <button type="submit" class="todo__item-delete">削除</button>
                 </form>
             </li>
-            <li class="todo__item">
-                <form action="/todos/update" method="post" class="todo__update-form">
-                    @csrf
-                    <input type="text" name="content" value="test2" class="todo__item-input">
-                    <button type="submit" class="todo__item-update">更新</button>
-                </form>
-                <form action="/todos/delete" method="post" class="todo__delete-form">
-                    @csrf
-                    <button type="submit" class="todo__item-delete">削除</button>
-                </form>
-            </li>
+            @endforeach
         </ul>
     </div>
 </div>
@@ -61,7 +52,8 @@
 </div>
 
 <div class="todo__content">
-    <form class="create-form">
+    <form class="create-form" action="/todos" method="post">
+        @csrf
         <div class="create-form__item">
             <input class="create-form__item-input" type="text" name="content">
         </div>
@@ -74,11 +66,12 @@
             <tr class="todo-table__row">
                 <th class="todo-table__header">Todo</th>
             </tr>
+            @foreach ($todos as $todo)
             <tr class="todo-table__row">
                 <td class="todo-table__item">
                     <form class="update-form">
                         <div class="update-form__item">
-                            <input class="update-form__item-input" type="text" name="content" value="test">
+                            <input class="update-form__item-input">{{ $todo['content'] }}</input>
                         </div>
                         <div class="update-form__button">
                             <button class="update-form__button-submit" type="submit">更新</button>
@@ -93,25 +86,7 @@
                     </form>
                 </td>
             </tr>
-            <tr class="todo-table__row">
-                <td class="todo-table__item">
-                    <form class="update-form">
-                        <div class="update-form__item">
-                            <input class="update-form__item-input" type="text" name="content" value="test2">
-                        </div>
-                        <div class="update-form__button">
-                            <button class="update-form__button-submit" type="submit">更新</button>
-                        </div>
-                    </form>
-                </td>
-                <td class="todo-table__item">
-                    <form class="delete-form">
-                        <div class="delete-form__button">
-                            <button class="delete-form__button-submit" type="submit">削除</button>
-                        </div>
-                    </form>
-                </td>
-            </tr>
+            @endforeach
         </table>
     </div>
 </div>
--- todo/src/routes/web.php
+++ todo/src/routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 
 Route::get('/', [TodoController::class, 'index']);
+Route::post('/todos', [TodoController::class, 'store']);
